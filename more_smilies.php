09f7aa15d13aa97a8c24fd0c931e0e0a7502859a
Heiko August <post@auge8472.de>
2009-12-10 19:46:44 +0100

    Refactoring: more_smilies.php aufgeräumt

--- more_smilies.php
+++ more_smilies.php
@@ -10,9 +10,12 @@ include("inc.php");
 <?php
 $result = mysql_query("SELECT file, code_1, title FROM ".$db_settings['smilies_table']." ORDER BY order_id ASC", $connid);
 while ($data = mysql_fetch_array($result))
- {
-  ?><a href="#" title="<?php echo $lang['smiley_title']; ?>" onclick="opener.insert('<?php echo stripslashes($data['code_1']); ?> '); window.close();"><img style="margin: 0px 10px 10px 0px; border: 0px;" src="img/smilies/<?php echo stripslashes($data['file']); ?>" alt="<?php echo stripslashes($data['code_1']); ?>" /></a><?php
- }
+	{
+	echo '<a href="#" title="'.$lang['smiley_title'].'" onclick="opener.insert(\'';
+	echo stripslashes($data['code_1']).'\'); window.close();">';
+	echo '<img style="margin: 0px 10px 10px 0px; border: 0px;" src="img/smilies/';
+	echo stripslashes($data['file']).'" alt="'.stripslashes($data['code_1']).'" /></a>'."\n";
+	}
 mysql_free_result($result);
 ?>
 </body>
