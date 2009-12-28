5170447ca1dffb2138c48c3bcc26ced79820d807
Heiko August <post@auge8472.de>
2009-12-28 23:50:26 +0100

    Refactoring: Ein paar Abfragen umgestellt, Coding Style

--- inc.php
+++ inc.php
@@ -52,25 +52,25 @@ if (basename($_SERVER['SCRIPT_NAME'])!='login.php'
 && $_SESSION[$settings['session_prefix'].'user_type']=='admin'))
 && $settings['forum_disabled']==1)
 	{
-	if(isset($_SESSION[$settings['session_prefix'].'user_type']) && $_SESSION[$settings['session_prefix'].'user_type'] != 'admin')
+	if (isset($_SESSION[$settings['session_prefix'].'user_type']) && $_SESSION[$settings['session_prefix'].'user_type'] != 'admin')
 		{
 		session_destroy();
-		setcookie("auto_login","",0);
+		setcookie("auto_login", "", 0);
 		}
-	header("location: ".$settings['forum_address']."info.php?info=1");
-	die("<a href=\"info.php?info=1\">further...</a>");
+	header('location: '.$settings['forum_address'].'info.php?info=1');
+	die('<a href="info.php?info=1">further...</a>');
 	}
 
 // look if IP is banned:
 $ip_result = mysql_query("SELECT list FROM ".$db_settings['banlists_table']." WHERE name = 'ips' LIMIT 1", $connid);
-if(!$ip_result) die($lang['db_error']);
-$data = mysql_fetch_array($ip_result);
+if (!$ip_result) die($lang['db_error']);
+$data = mysql_fetch_assoc($ip_result);
 mysql_free_result($ip_result);
 
 if (trim($data['list']) != '')
 	{
-	$banned_ips_array = explode(',',trim($data['list']));
-	if (in_array($_SERVER["REMOTE_ADDR"],$banned_ips_array))
+	$banned_ips_array = explode(',', trim($data['list']));
+	if (in_array($_SERVER["REMOTE_ADDR"], $banned_ips_array))
 		{
 		die($lang['ip_no_access']);
 		}
@@ -79,17 +79,17 @@ if (trim($data['list']) != '')
 // look if user is banned:
 if (isset($_SESSION[$settings['session_prefix'].'user_name']))
 	{
-	$ban_result=mysql_query("SELECT list FROM ".$db_settings['banlists_table']." WHERE name = 'users' LIMIT 1", $connid);
-	if(!$ban_result) die($lang['db_error']);
-	$data = mysql_fetch_array($ban_result);
+	$ban_result = mysql_query("SELECT list FROM ".$db_settings['banlists_table']." WHERE name = 'users' LIMIT 1", $connid);
+	if (!$ban_result) die($lang['db_error']);
+	$data = mysql_fetch_assoc($ban_result);
 	mysql_free_result($ban_result);
 	if (trim($data['list']) != '')
 		{
-		$banned_users_array = explode(',',strtolower(trim($data['list'])));
-		if (in_array(strtolower($_SESSION[$settings['session_prefix'].'user_name']),$banned_users_array) && $_SESSION[$settings['session_prefix'].'user_type']!='admin')
+		$banned_users_array = explode(',', mb_strtolower(trim($data['list'])));
+		if (in_array(mb_strtolower($_SESSION[$settings['session_prefix'].'user_name']),$banned_users_array) && $_SESSION[$settings['session_prefix'].'user_type']!='admin')
 			{
 			session_destroy();
-			setcookie("auto_login","",0);
+			setcookie("auto_login", "", 0);
 			header("location: ".$settings['forum_address']."login.php?msg=user_banned");
 			die($lang['user_banned']);
 			}
@@ -97,38 +97,40 @@ if (isset($_SESSION[$settings['session_prefix'].'user_name']))
 	}
 
 // determine last visit:
-if (empty($_SESSION[$settings['session_prefix']."user_id"]) && $settings['remember_last_visit'] == 1)
+if (empty($_SESSION[$settings['session_prefix']."user_id"])
+&& $settings['remember_last_visit'] == 1)
 	{
 	if (isset($_COOKIE['last_visit']))
 		{
 		$c_last_visit = explode(".", $_COOKIE['last_visit']);
-		if (isset($c_last_visit[0])) $c_last_visit[0] = trim($c_last_visit[0]);
-		else $c_last_visit[0] = time();
-		if (isset($c_last_visit[1])) $c_last_visit[1] = trim($c_last_visit[1]);
-		else $c_last_visit[1] = time();
+		$c_last_visit[0] = (isset($c_last_visit[0])) ? trim($c_last_visit[0]) : time();
+		$c_last_visit[1] = (isset($c_last_visit[1])) ? trim($c_last_visit[1]) : time();
 		if ($c_last_visit[1] < (time() - 600))
 			{
 			$c_last_visit[0] = $c_last_visit[1];
 			$c_last_visit[1] = time();
-			setcookie("last_visit",$c_last_visit[0].".".$c_last_visit[1],time()+(3600*24*30));
+			setcookie("last_visit", $c_last_visit[0].".".$c_last_visit[1], time()+(3600*24*30));
 			}
 		}
-	else setcookie("last_visit",time().".".time(),time()+(3600*24*30));
+	else
+		{
+		setcookie("last_visit", time().".".time(), time()+(3600*24*30));
+		}
 	}
 
-if (isset($c_last_visit)) $last_visit = $c_last_visit[0];
-else $last_visit = time();
+$last_visit = (isset($c_last_visit)) ? $c_last_visit[0] : time();
 
 if (isset($_GET['category'])) $category = intval($_GET['category']);
 $categories = get_categories();
 $category_ids = get_category_ids($categories);
-if($category_ids!=false) $category_ids_query = implode(", ", $category_ids);
-if(empty($category)) $category=0;
+if ($category_ids != false) $category_ids_query = implode(", ", $category_ids);
+if (empty($category)) $category=0;
 
 if (isset($_SESSION[$settings['session_prefix'].'user_id'])) $category_accession = category_accession();
 
-// count postings, threads, users and users online:
-if ($categories == false) // no categories defined
+# count postings, threads, users and users online:
+# no categories defined
+if ($categories == false)
 	{
 	$count_result = mysql_query("SELECT COUNT(*) FROM ".$db_settings['forum_table']." WHERE pid = 0", $connid);
 	list($thread_count) = mysql_fetch_row($count_result);
@@ -137,10 +139,9 @@ if ($categories == false) // no categories defined
 	list($posting_count) = mysql_fetch_row($count_result);
 	mysql_free_result($count_result);
 	}
-else if (is_array($categories)) // there are categories
+# there are categories
+else if (is_array($categories))
 	{
-	#foreach($categories as $part) $request_categories[] = "'".mysql_escape_string($part)."'";
-	#$request_categories = implode(", ", $request_categories).", ''";
 	$count_result = mysql_query("SELECT COUNT(*) FROM ".$db_settings['forum_table']." WHERE pid = 0 AND category IN (".$category_ids_query.")", $connid);
 	list($thread_count) = mysql_fetch_row($count_result);
 	mysql_free_result($count_result);
@@ -173,17 +174,23 @@ if ($settings['count_users_online'] == 1)
 	}
 else
 	{
-	$counter = str_replace("[forum_name]", "<a href=\"".$settings['forum_address']."\">".stripslashes($settings['forum_name'])."</a>", $lang['counter']);
-	$counter = str_replace("[contact]", "<a href=\"contact.php?forum_contact=true\">".$lang['contact_linkname']."</a>", $counter);
+	$counter = str_replace("[forum_name]", '<a href="'.$settings['forum_address'].'">'.$settings['forum_name'].'</a>', $lang['counter']);
+	$counter = str_replace("[contact]", '<a href="contact.php?forum_contact=true">'.$lang['contact_linkname'].'</a>', $counter);
 	$counter = str_replace("[postings]", $posting_count, $counter);
 	$counter = str_replace("[threads]", $thread_count, $counter);
 	$counter = str_replace("[users]", $user_count, $counter);
 	}
 mysql_free_result($count_result);
 
-if (isset($settings['time_difference'])) $time_difference = $settings['time_difference'];
-else $time_difference = 0;
+$time_difference = (isset($settings['time_difference'])) ? $settings['time_difference'] : 0;
 
-if (isset($_SESSION[$settings['session_prefix'].'user_time_difference'])) $time_difference = $_SESSION[$settings['session_prefix'].'user_time_difference']+$time_difference;
-else if (isset($_COOKIE['user_time_difference'])) $time_difference = $_COOKIE['user_time_difference']+$time_difference;
+if (isset($_SESSION[$settings['session_prefix'].'user_time_difference']))
+	{
+	$time_difference = $_SESSION[$settings['session_prefix'].'user_time_difference']+$time_difference;
+	}
+else if (isset($_COOKIE['user_time_difference']))
+	{
+	$time_difference = $_COOKIE['user_time_difference']+$time_difference;
+	}
+
 ?>
