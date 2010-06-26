b8742157ccb6fface93a8c298bbe33e1c225a96b
Heiko August <post@auge8472.de>
2010-06-26 12:00:31 +0200

    Code: Info "Forum in Wartung" in die info.php eingefügt

--- info.php
+++ info.php
@@ -9,11 +9,13 @@ $topnav = '<img src="img/where.gif" alt="" width="11" height="8" /><b>'.$lang['i
 parse_template();
 echo $header;
 ?>
+<h2><?php echo $lang['info']; ?></h2>
 <p><?php
 switch ($info)
 	{
 	case 0; echo '&nbsp;'; break;
 	case 1: echo $lang['info_forum_disabled']; break;
+	case 2: echo $lang['info_forum_servicing']; break;
 	default: echo '&nbsp;'; break;
 	}
 ?></p>
