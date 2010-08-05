f9851413ea4064ae5c7465643a6641d58ce15c43
Heiko August <post@auge8472.de>
2010-08-05 19:45:28 +0200

    Bugfix: Aufrufe der mb-Funktionen aus den gleichnamigen mb-Ersatzfunktionen entfernt, ein paar Optimierungen

--- functions/funcs.mb_replacements.php
+++ functions/funcs.mb_replacements.php
@@ -1,49 +1,58 @@
 <?php
-
+/*
 function mb_internal_encoding($charset='') {
 }
-
+*/
 function mb_strlen($string, $encoding='utf-8') {
 $encoding = strtolower($encoding);
-if (function_exists('mb_strlen'))
-	{
-	return mb_strlen($string, $encoding);
-	}
-else if ($encoding=='utf-8')
+if ($encoding=='utf-8')
 	{
 	$string = utf8_decode($string);
-	return = strlen($string);
+	}
+return = strlen($string);
+}
+
+function mb_substr($string, $start, $length=0, $encoding='utf-8') {
+$encoding = strtolower($encoding);
+if ($encoding=='utf-8')
+	{
+	$string = utf8_decode($string);
+	$string = substr($string, $start, $length);
+	$string = utf8_encode($string);
+	return $string;
 	}
 else
 	{
-	return strlen($string);
+	return substr($string, $start, $length);
 	}
 }
 
-function mb_substr($str, $start, $length=0, $encoding='') {
-return substr($str, $start, $length);
-}
-
-function mb_strpos($haystack, $needle, $offset=0, $encoding='') {
+function mb_strpos($haystack, $needle, $offset=0, $encoding='utf-8') {
+$encoding = strtolower($encoding);
+if ($encoding=='utf-8')
+	{
+	$haystack = utf8_decode($haystack);
+	}
 return strpos($haystack, $needle, $offset);
 }
 
-function mb_strrpos($haystack, $needle, $offset=0, $encoding='') {
+function mb_strrpos($haystack, $needle, $offset=0, $encoding='utf-8') {
+$encoding = strtolower($encoding);
+if ($encoding=='utf-8')
+	{
+	$haystack = utf8_decode($haystack);
+	}
 return strrpos($haystack, $needle, $offset);
 }
 
 function mb_strtolower($string, $encoding='utf-8') {
 $encoding = strtolower($encoding);
-if (function_exists('mb_strtolower'))
-	{
-	return mb_strtolower($string, $encoding);
-	}
-else if ($encoding=='utf-8')
+if ($encoding=='utf-8')
 	{
 	$string = utf8_decode($string);
 	$string = strtolower($string);
 	$string = utf8_encode($string);
-	return ($string);
+	return $string;
 	}
 else
 	{
@@ -53,16 +62,12 @@ else
 
 function mb_strtoupper($string, $encoding='utf-8') {
 $encoding = strtolower($encoding);
-if (function_exists('mb_strtolower'))
-	{
-	return mb_strtoupper($string, $encoding);
-	}
-else if ($encoding=='utf-8')
+if ($encoding=='utf-8')
 	{
 	$string = utf8_decode($string);
 	$string = strtoupper($string);
 	$string = utf8_encode($string);
-	return ($string);
+	return $string;
 	}
 else
 	{
