3b383f86b09a84fd9fb5d9336369cff24390b0ff
Heiko August <post@auge8472.de>
2010-10-27 20:43:18 +0200

    Code: neu: include.prepare.php; nimmt Code auf, der bei allen aufrufbaren Skripten am Anfang ausgeführt werden soll

---
+++ functions/include.prepare.php
@@ -0,0 +1,34 @@
+<?php
+###############################################################################
+# my little forum                                                             #
+# Copyright (C) 2005 Alex                                                     #
+# http://www.mylittlehomepage.net/                                            #
+#                                                                             #
+# This program is free software; you can redistribute it and/or               #
+# modify it under the terms of the GNU General Public License                 #
+# as published by the Free Software Foundation; either version 2              #
+# of the License, or (at your option) any later version.                      #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program; if not, write to the Free Software                 #
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. #
+###############################################################################
+
+/**
+ * @author: Heinko August
+ * @email: post@auge8472.de
+ */
+
+if (file_exists('install.php')
+or file_exists('update.php'))
+	{
+	header("location: ".$settings['forum_address']."info.php?info=2");
+	die("<a href=\"info.php?info=2\">further...</a>");
+	}
+
+?>
