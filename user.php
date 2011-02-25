de8131683d82e8359b1e3cfc8b301b47d60eea0a
Heiko August <post@auge8472.de>
2011-02-25 14:17:11 +0100

    Fix: Codestyle, Einrückung per Tab anstatt Leerzeichen

--- user.php
+++ user.php
@@ -1280,7 +1280,7 @@ switch ($action)
 			echo $lang['pers_msg_deactivated'];
 			}
 	break;
-   case "locked":
+	case "locked":
 		echo '<h2 class="caution">'.$lang['user_locked_hl'].'</h2>'."\n";
 		echo '<p>'.str_replace("[name]", htmlspecialchars($user_name), $lang['usr_locked_txt']).'</p>'."\n";
 	break;
