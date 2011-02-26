817070818b5b567cea53976768e939e75ef96b30
Heiko August <post@auge8472.de>
2011-02-26 17:11:36 +0100

    Bugfix: Auslesen der Postingzeit und des Zeitpunkts der letzten Antwort als Unix timestamp
    Die Werte werden zur Ermittlung von Antworten seit dem letzten Besuch benötigt.

--- forum.php
+++ forum.php
@@ -142,6 +142,8 @@ if ($settings['access_for_users_only'] == 1
 			tid,
 			t1.user_id AS posters_id,
 			DATE_FORMAT(time + INTERVAL ".$time_difference." HOUR, '".$lang['time_format_sql']."') AS Uhrzeit,
+			UNIX_TIMESTAMP(time + INTERVAL ".$time_difference." HOUR) AS time,
+			UNIX_TIMESTAMP(last_answer + INTERVAL ".$time_difference." HOUR) AS last_answer,
 			name,
 			subject,
 			category,
