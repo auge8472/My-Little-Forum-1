1c85abb752a7e951eacb1806f0d8803e50208835
Heiko August <post@auge8472.de>
2011-10-31 04:20:59 +0100

    Code: generierten HTML-Code mit Umbrüchen (<br />) angereichert
    Die fehlenden Umbrüche führten z.B. im Opera 11.52 dazu, dass die betreffenden Buttons neben- statt untereinander angeordnet wurden.

--- data/loader.js
+++ data/loader.js
@@ -6,11 +6,11 @@ if ($('buttonspace'))
 	{
 	bbCodeButtons(auge_buttons);
 
-	Element.insert($('buttonspace'), {'bottom': " <input value=\"Link\" title=\"Link einfügen: \[link=http://www.domain.tld/\]Link\[/link\] oder \[link\]http://www.domain.tld/\[/link\]\" class=\"bb-button\" type=\"button\" name=\"link2\" onclick=\"insert_link('entryform','text','Link-Text (optional)','Link-Ziel (URL):');\" />"});
+	Element.insert($('buttonspace'), {'bottom': "<input value=\"Link\" title=\"Link einfügen: \[link=http://www.domain.tld/\]Link\[/link\] oder \[link\]http://www.domain.tld/\[/link\]\" class=\"bb-button\" type=\"button\" name=\"link2\" onclick=\"insert_link('entryform','text','Link-Text (optional)','Link-Ziel (URL):');\" /><br />\n"});
 
 	if (typeof(auge_upload) != "undefined")
 		{
-		Element.insert($('buttonspace'), {'bottom': " <input value=\""+ auge_upload.get('text') +"\" title=\""+ auge_upload.get('title') +"\" class=\"bb-button\" type=\"button\" name=\"imgupload\" onclick=\"createPopup('upload.php', 380, 340);\" />"});
+		Element.insert($('buttonspace'), {'bottom': "<input value=\""+ auge_upload.get('text') +"\" title=\""+ auge_upload.get('title') +"\" class=\"bb-button\" type=\"button\" name=\"imgupload\" onclick=\"createPopup('upload.php', 380, 340);\" /><br />\n"});
 		}
 	if (typeof(auge_smilies) != "undefined")
 		{
