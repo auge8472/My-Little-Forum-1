5d519de0e0ef85284082b65e88d5c554532332b0
Heiko August <post@auge8472.de>
2012-07-01 20:39:17 +0200

    Code: Die Behandlung von Sessions von der get_magic_quotes_gpc-Abfrage ausgenommen

--- inc.php
+++ inc.php
@@ -45,7 +45,6 @@ if (get_magic_quotes_gpc())
 	$_POST = array_map('stripslashes_deep', $_POST);
 	$_GET = array_map('stripslashes_deep', $_GET);
 	$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-	$_SESSION = array_map('stripslashes_deep', $_SESSION);
 	$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
 	}
 
