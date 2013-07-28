726f5588d979dea5a7e1011e5e3bb0b1cc12248f
Heiko August <post@auge8472.de>
2013-07-29 00:26:51 +0200

    Code: generate output of posting text for the preview

--- posting.php
+++ posting.php
@@ -1172,6 +1172,18 @@ if (($settings['access_for_users_only'] == 1
 					$mark['mod'] = false;
 					$mark['user'] = false;
 					$prAuthorinfo = outputAuthorInfo($mark, $_POST, 0, 0, $isView);
+					if (empty($_POST['text']))
+						{
+						$prText = $lang['no_text'];
+						}
+					else
+						{
+						$prText = $_POST['text'];
+						$prText = ($settings['autolink'] == 1) ? make_link($prText) : $prText;
+						$prText = ($settings['bbcode'] == 1) ? bbcode($prText) : $prText;
+						$prText = ($settings['smilies'] == 1) ? smilies($prText) : $prText;
+						$prText = zitat($prText);
+						}
 					} # End: if (isset($preview) && empty($errors))
 			break;
 			# End: switch ($show)->case "form"
