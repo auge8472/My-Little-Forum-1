ec97b71c2d7a13bcec48b4d8bbae94aca3a3e275
Heiko August <post@auge8472.de>
2013-07-29 00:31:32 +0200

    Code: generate output of signature for the preview

--- posting.php
+++ posting.php
@@ -1184,6 +1184,20 @@ if (($settings['access_for_users_only'] == 1
 						$prText = ($settings['smilies'] == 1) ? smilies($prText) : $prText;
 						$prText = zitat($prText);
 						}
+					if ($show_signature == 1
+					&& !empty($previewUserData['signature']))
+						{
+						$prSignature = $previewUserData['signature'];
+						$prSignature = $settings['signature_separator']."\n".$prSignature;
+						$prSignature = ($settings['autolink'] == 1) ? make_link($prSignature) : $prSignature;
+						$prSignature = ($settings['bbcode'] == 1) ? bbcode($prSignature) : $prSignature;
+						$prSignature = ($settings['smilies'] == 1) ? smilies($prSignature) : $prSignature;
+						$prSignature = '<div class="signature">'. $prSignature .'</div>'."\n";
+						}
+					else
+						{
+						$prSignature = '';
+						}
 					} # End: if (isset($preview) && empty($errors))
 			break;
 			# End: switch ($show)->case "form"
