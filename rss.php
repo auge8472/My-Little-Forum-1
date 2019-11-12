f0a15bd29c404fbc3b19d808e69e0753d29566d2
Heiko August <post@auge8472.de>
2019-11-12 18:25:36 +0100

    Change: move the XML-declaration from the PHP-code to the XML-template

--- rss.php
+++ rss.php
@@ -36,8 +36,7 @@ include("inc.php");
   }
 $result_count = mysqli_num_rows($result);
 header("Content-Type: text/xml; charset=utf-8");
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
+?><?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
 <channel>
 <title><?php echo $settings['forum_name']; ?></title>
