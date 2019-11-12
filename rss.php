505ab61573d4153aa1b05954813df91a88aa0105
Heiko August <post@auge8472.de>
2019-11-12 16:27:10 +0100

    Fix: wrong syntax for setting HTTP-header for charset

--- rss.php
+++ rss.php
@@ -35,7 +35,7 @@ include("inc.php");
    if(!$result) die($lang['db_error']);
   }
 $result_count = mysqli_num_rows($result);
-header("Content-Type: text/xml; charset:  utf-8");
+header("Content-Type: text/xml; charset=utf-8");
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
