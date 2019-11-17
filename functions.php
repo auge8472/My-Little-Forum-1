1d272179dc98edc59c5e305fea83531c06f45ce1
Heiko August <post@auge8472.de>
2019-11-17 01:57:01 +0100

    Change: lay the basis for themes
    - add a setting to specify the theme by the forum operator
    - move the main template into the standard theme directory
    - create a function to check the existence of the theme, specified in the settings and to return the theme path

--- functions.php
+++ functions.php
@@ -380,10 +380,17 @@ function user_online()
   ?></li><?php
  }
 
+function getThemeDir($settingTheme = false) {
+	if ($settingTheme === false) return false;
+	$theme = (is_dir('themes/'. $settingTheme .'/templates')) ? 'themes/'. $settingTheme : 'themes/mlf1-classic';
+	return $theme;
+}
+
 function parse_template()
  {
   global $settings, $lang, $header, $footer, $wo, $ao, $topnav, $subnav_1, $subnav_2, $footer_info_dump, $search, $show_postings, $counter;
-  $template = implode("",file($settings['template']));
+  $theme = getThemeDir($settings['theme']);
+  $template = file_get_contents($theme .'/templates/main.html');
 
   if ($settings['home_linkaddress'] != "" && $settings['home_linkname'] != "") $template = preg_replace("#\{IF:HOME-LINK\}(.+?)\{ENDIF:HOME-LINK\}#is", "\\1", $template);
   else $template = preg_replace("#\{IF:HOME-LINK\}(.+?)\{ENDIF:HOME-LINK\}#is", "", $template);
