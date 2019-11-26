9403c5380288b565a520cc68cd22d58d7096c3db
Heiko August <post@auge8472.de>
2019-11-26 02:35:56 +0100

    Change: reformatting of the file info.php

--- info.php
+++ info.php
@@ -23,8 +23,8 @@
 
 include("inc.php");
 
-if(isset($_GET['info'])) $info = intval($_GET['info']);
-if(empty($info)) $info = 0;
+if (isset($_GET['info'])) $info = intval($_GET['info']);
+if (empty($info)) $info = 0;
 
 $topnav = '<img src="'. $settings['themepath'] .'/img/where.gif" alt="" width="11" height="8" /><b>'.$lang['info'].'</b>';
 
@@ -33,12 +33,17 @@ echo $header;
 
 ?><p><?php
 
-switch ($info)
- {
-  case 0; echo '&nbsp;'; break;
-  case 1: echo $lang['info_forum_disabled']; break;
-  default: echo '&nbsp;'; break;
- }
+switch ($info) {
+	case 0;
+		echo '&nbsp;';
+	break;
+	case 1:
+		echo $lang['info_forum_disabled'];
+	break;
+	default:
+		echo '&nbsp;';
+	break;
+}
 
 ?></p><?php
 
